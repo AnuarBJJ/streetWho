f08fd172cef86199b1f77277758f65d59cec3d76
AnuarBJJ <anuar.zhukenov@gmail.com>
2016-08-01 16:59:38 -0700

    both swift and web get proper response

--- server/controllers/streets.js
+++ server/controllers/streets.js
@@ -11,7 +11,7 @@ module.exports = (function(){
 				} else {
 					console.log(result)
 					if(result){
-						res.send(result.info);
+						res.json(result);
 					}
 					else{
 						var newStreet = new Street({
